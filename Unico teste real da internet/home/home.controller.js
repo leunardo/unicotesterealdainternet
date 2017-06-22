dcb74ade9836b229a4eb3867f6d6024fdacdaa73
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-06-22 12:05:38 -0300

    Substituir formulario do nome por login com o google.

--- Unico teste real da internet/home/home.controller.js
+++ Unico teste real da internet/home/home.controller.js
@@ -1,10 +1,36 @@
 app.controller('homeController', homeController);
 
-function homeController ($scope, $location) {
-    $scope.avancar = avancar;
+function homeController($scope, $rootScope, $location) {
+    //$scope.avancar = avancar;
+    $rootScope.logged = false;
 
-    function avancar () {
-        localStorage.nome = $scope.usuario.nome;
-        $location.path('/quiz');
+
+    function signOut() {
+
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+
+            $rootScope.logged = false;
+
+            $location.path('/index');
+            $scope.$apply();
+        });
+
     }
+
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        localStorage.nome = profile.getName();
+
+        $rootScope.logged = true;
+        $location.path('/quiz');
+        $scope.$apply();
+
+        //todo: redirecionar para todos a lista de quizes
+        
+    }
+
+    window.signOut = signOut;
+    window.onSignIn = onSignIn;
+
 }
