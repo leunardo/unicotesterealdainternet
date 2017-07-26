a74ed43f0dca68c389c65c2d28a18b5029d2a09f
Leonardo Alves <leunardosevla@gmail.com>
2017-07-26 00:11:48 -0300

    Remover código  da home

--- Unico teste real da internet/home/home.controller.js
+++ Unico teste real da internet/home/home.controller.js
@@ -1,36 +1,3 @@
 app.controller('homeController', homeController);
 
-function homeController($scope, $rootScope, $location) {
-    //$scope.avancar = avancar;
-    $rootScope.logged = false;
-
-
-    function signOut() {
-
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-
-            $rootScope.logged = false;
-
-            $location.path('/index');
-            $scope.$apply();
-        });
-
-    }
-
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        localStorage.nome = profile.getName();
-
-        $rootScope.logged = true;
-        $location.path('/quiz');
-        $scope.$apply();
-
-        //todo: redirecionar para todos a lista de quizes
-        
-    }
-
-    window.signOut = signOut;
-    window.onSignIn = onSignIn;
-
-}
+function homeController($scope, $location, authService) {}
