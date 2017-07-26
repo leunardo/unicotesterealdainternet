686b87512e4c105a590ec2e29bc9e55825a27853
Leonardo Alves <leunardosevla@gmail.com>
2017-07-26 12:19:27 -0300

    adicionar visualizaçao basica do usuario

--- Unico teste real da internet/js/auth.controller.js
+++ Unico teste real da internet/js/auth.controller.js
@@ -3,8 +3,10 @@ app.controller('authController', authController);
 function authController($scope, $interval, authService) {   
     $scope.signIn = authService.signIn;
     $scope.signOut = authService.signOut;
-
     $scope.$watch(
         () =>  authService.isLogado(),
-        () => { $scope.logged = authService.isLogado() });
+        () => { 
+            $scope.logged = authService.isLogado();
+            $scope.usuario = JSON.parse(localStorage.usuario);             
+        });
 }
