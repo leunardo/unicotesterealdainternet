63f7172b971161abfc82c21359223a266eebb698
Leonardo Alves <leunardosevla@gmail.com>
2017-07-27 12:30:36 -0300

    wip: Controller de criação de pergunta.

--- Unico teste real da internet/criacaoQuiz/criacaoQuiz.controller.js
+++ Unico teste real da internet/criacaoQuiz/criacaoQuiz.controller.js
@@ -1,26 +1,38 @@
 app.controller('criacaoQuizController', criacaoQuizController);
 
-function criacaoQuizController($scope, criacaoPerguntasService){
-    $scope.resposta = []
-    $scope.oi = ".";
-    $scope.criarPergunta = criarPergunta;
+function criacaoQuizController($scope, criacaoQuizService){
+    $scope.respostas = []
+    // $scope.proximaPergunta = proximaPergunta;
+    // $scope.criarPergunta = criarPergunta;
+    $scope.adicionarResposta = adicionarResposta;
+    $scope.removerResposta = removerResposta;
 
-    function criarPergunta(){
-        if($scope.pergunta!=""){
-            var pergunta = {
-                pergunta: $scope.pergunta,
-                respostas: []
-            }
-            for(n = 0; n<5; n++){
-                if($scope.resposta[n]==""){
-                    scope.resposta[n]=null;
-                }
-            }
-            pergunta.respostas = $scope.resposta;
-            criacaoPerguntasService.criarPergunta(pergunta).then(r => {
-               $scope.pergunta = "";
-               $scope.resposta = [];
-            });
-        }
+    // function criarPergunta(){
+    //     if($scope.pergunta!=""){
+    //         var pergunta = {
+    //             pergunta: $scope.pergunta,
+    //             respostas: []
+    //         }
+    //         for(n = 0; n<5; n++){
+    //             if($scope.resposta[n]==""){
+    //                 scope.resposta[n]=null;
+    //             }
+    //         }
+    //         pergunta.respostas = $scope.resposta;
+    //         criacaoPerguntasService.criarPergunta(pergunta).then(r => {
+    //            $scope.pergunta = "";
+    //            $scope.resposta = [];
+    //         });
+    //     }
+    // }
+
+    function adicionarResposta() {
+        let copia = angular.copy($scope.resposta);
+        $scope.resposta = '';
+        $scope.respostas.push(copia);
     }
-}
+
+    function removerResposta(indice) {
+        $scope.respostas.splice(indice, 1);
+    }
+ }
