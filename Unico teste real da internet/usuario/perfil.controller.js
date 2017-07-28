1005bcca8b83ff4159a900b4f1ca3a7ba51388d9
Unknown <unknown@example.com>
2017-07-28 10:23:32 -0300

    Adicionar  função para atualizar perfil.

--- Unico teste real da internet/usuario/perfil.controller.js
+++ Unico teste real da internet/usuario/perfil.controller.js
@@ -3,11 +3,14 @@ app.controller('perfilController', perfilController);
 function perfilController($scope, $routeParams, usuarioService, quizService) {
     getUsuario();
     getQuizzesDoUsuario();
-
+    $scope.checarUser = checarUser;
+    $scope.usuario = "";
+    $scope.atualizarPerfil = atualizarPerfil;
     function getUsuario() {
         usuarioService.getUsuarioPorId($routeParams.id)
             .then(response => {
-                $scope.usuario = response.data;
+                $scope.usuario = response.data; 
+                $scope.usuarioCopia = angular.copy($scope.usuario);
             }, fail => {
                 alert("Deu erro");
             });
@@ -20,5 +23,18 @@ function perfilController($scope, $routeParams, usuarioService, quizService) {
             }, fail => {
                 alert("deu erro");
             })
+    }
+
+    function checarUser(){
+        return $scope.usuario.id === JSON.parse(localStorage.usuario).id;           
+    }
+
+    function atualizarPerfil(){
+        usuarioService.atualizarPerfil($scope.usuarioCopia)
+            .then(function response(resposta){
+                $scope.usuario = angular.copy($scope.usuarioCopia);
+            }, function erro(error) {
+                console.log(error);
+            });
     }
 }
