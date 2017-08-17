30f2341861afdaba13744d310663c4a7e8cd9e1a
Leonardo Alves <leunardosevla@gmail.com>
2017-08-17 08:38:06 -0300

    Arquivos movidos

--- Unico teste real da internet/resultado/resultado.controller.js
+++
@@ -1,6 +0,0 @@
-app.controller("resultadoController", resultadoController);
-
-function resultadoController($scope, quizService){
-    $scope.QI = quizService.getQI;
-    $scope.nome = JSON.parse(localStorage.usuario).nome;
-}
