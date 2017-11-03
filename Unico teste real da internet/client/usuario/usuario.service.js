79c665e579c51d04966e9a509bcb6aa4a9ea4d4c
guilhermeLS <guilhermejardimpereira@gmail.com>
2017-11-02 23:15:27 -0200

    quick fix para edição do perfil

---
+++ Unico teste real da internet/client/usuario/usuario.service.js
@@ -0,0 +1,33 @@
+app.service('usuarioService', usuarioService);
+
+function usuarioService($http, URL) {
+    let url = URL;
+
+    return {
+        getUsuario: getUsuario,
+        criarUsuario: criarUsuario,
+        getUsuarioPorId: getUsuarioPorId,
+        atualizarPerfil: atualizarPerfil,
+        usuarioJaCadastrado: usuarioJaCadastrado
+    }
+
+    function getUsuario() {
+        return $http.get(`${url}/usuarios`);
+    }
+
+    function getUsuarioPorId(id) {
+        return $http.get(`${url}/usuarios/${id}`);
+    }
+
+    function criarUsuario(usuario) {
+        return $http.post(`${url}/usuarios`, usuario);
+    }
+
+    function usuarioJaCadastrado(id_google) {
+        return $http.get(`${url}/usuarios/gid/${id_google}`);
+    }
+
+    function atualizarPerfil(usuario, token){    
+        return $http.put(`${url}/usuarios/${usuario.id_usuario}`, [usuario, token]);
+    }
+}
