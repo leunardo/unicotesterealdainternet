7b24260f53fc0c397b2dc99e94d7fc4c3bfddf53
Leonardo Alves <leunardosevla@gmail.com>
2017-11-05 13:59:13 -0200

    Alterar calculo de proximidade, formatar exemplos

--- Unico teste real da internet/friendship-ml/proximidade.py
+++ Unico teste real da internet/friendship-ml/proximidade.py
@@ -1,7 +1,7 @@
 import numpy as np
 from user import User
 from similaridadecossenos import similaridade_cossenos
-
+from similaridadejaccard import similaridade_amigos, similaridade_quizzes_feitos
 
 def calcular_proximidade(usuario_1, usuario_2):
     """Calcula a proximidade entre um usuario e outro.
@@ -10,13 +10,11 @@ def calcular_proximidade(usuario_1, usuario_2):
     :param usuario_2: Usuario 2.
     :return: Um ponto flutuante representando a proximidade dos dois usuarios.
     """
+    quizzes = similaridade_quizzes_feitos(usuario_1, usuario_2)
+    amigos  = similaridade_amigos(usuario_1, usuario_2)
+    tags    = similaridade_cossenos(usuario_1.tags_em_comum(usuario_2))
 
-    amigos = usuario_1.amigos_em_comum(usuario_2)
-    quizzes = usuario_1.quizzes_em_comum(usuario_2)
-    tags = usuario_1.tags_em_comum(usuario_2)
-    valores_tags = list(tags.values()) or 0
-
-    proximidade = (len(amigos) ** 1.5 +  np.mean(valores_tags) + len(quizzes)) / 10
+    proximidade =  quizzes + amigos + tags
     return proximidade
 
 
@@ -34,14 +32,20 @@ def calcular_softmax(usuario_principal, usuarios):
 
 if __name__ == '__main__':
     """Executa o arquivo e roda amostras de teste."""
-    leo = User(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [], [10, 20, 30, 40, 50, 60, 70, 80], {"arroz": 4, "feijao": 3, "c": 2})
-    bruno = User(2, [2], [10, 20, 30, 40], [], {"arroz": 2, "feijao": 3, "batata": 6, "queijo": 3})
-    airton = User(3, [2, 3], [10, 20, 30, 40, 50, 60, 70], [], {"arroz": 8, "feijao": 3, "batata": 6})
-    pablo = User(4, [2, 3, 4], [10, 20, 30], [], {"arroz": 2, "feijao": 3, "batata": 6})
-    juck = User(5, [2, 3, 4], [10, 20, 30, 40], [], {"arroz": 2, "feijao": 3, "batata": 6})
-    jose = User(6, [2, 3, 4, 5, 6], [], [], {"calabresa": 3,  "sucao": 6})
-    # users = [bruno, jose, airton, pablo, juck]
-    # resultado = calcular_softmax(leo, users)
-    matriz_tags = leo.tags_em_comum(bruno)
-    print(similaridade_cossenos(matriz_tags))
-    #print(list(zip([user.uid for user in users], resultado)))
+    leo    = User(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [                          ], [10, 20, 30, 40, 50, 60, 70, 80], {"arroz": 4, "feijao": 3, "c": 2})
+    bruno  = User(2, [2                            ], [10, 20, 30, 40            ], [                              ], {"arroz": 2, "feijao": 3, "batata": 6, "queijo": 3})
+    airton = User(3, [2, 3                         ], [10, 20, 30, 40, 50, 60, 70], [                              ], {"arroz": 8, "feijao": 3, "batata": 6})
+    pablo  = User(4, [2, 3, 4                      ], [10, 20, 30                ], [                              ], {"arroz": 2, "feijao": 3, "batata": 6})
+    juck   = User(5, [2, 3, 4                      ], [10, 20, 30, 40            ], [                              ], {"arroz": 2, "feijao": 3, "batata": 6})
+    jose   = User(6, [2, 3, 4, 5, 6                ], [                          ], [                              ], {"calabresa": 3,  "sucao": 6})
+    #             ^    ^                                ^                             ^                                 ^
+    #            uid   amigos                          quizzes_respondidos           meus_quizzes                       tags
+    users = [bruno, jose, airton, pablo, juck]
+    resultado = calcular_softmax(leo, users)
+
+    print(list(zip([user.uid for user in users], resultado)))
+    for r in resultado:
+        if r > (1 / len(resultado)):
+            print(r, " tem coisas em comum com voce")
+        else:
+            print(r, " não tem coisas em comum com voce")
