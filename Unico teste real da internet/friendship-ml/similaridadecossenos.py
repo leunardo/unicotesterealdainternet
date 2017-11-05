39af0e4533de4dc9c15dcf194375675932ee9018
Leonardo Alves <leunardosevla@gmail.com>
2017-11-05 13:59:32 -0200

    Mover funcoes, adicionar documentacao

--- Unico teste real da internet/friendship-ml/similaridadecossenos.py
+++ Unico teste real da internet/friendship-ml/similaridadecossenos.py
@@ -1,20 +1,15 @@
+"""Esse modulo e reponsavel pelo calculo da similaridade entre cossenos."""
 import numpy as np
-
-
-def ordenar_dict(*args):
-    ret = []
-    for _dict in args:
-        sorted_items = [item[1] for item in sorted(_dict.items(), key=lambda x: x[0])]
-        ret.append(sorted_items)
-    return ret
-
-
-def raiz_quadrada(x):
-    return np.sqrt(np.sum([a * a for a in x]))
+from util import soma_raiz_quadrada
 
 
 def similaridade_cossenos(matriz):
+    """Calcula a similaridade entre cossenos em uma matriz com 2 linhas e n colunas.
+
+    :param matriz: Uma matriz 2,n
+    :return: Um numero [0,1] representando a similaridade entre as duas linhas.
+    """
     numerador = np.sum(a * b for a, b in zip(matriz[0], matriz[1]))
-    denominador = raiz_quadrada(matriz[0]) * raiz_quadrada(matriz[1]) + 1
+    denominador = (soma_raiz_quadrada(matriz[0]) * soma_raiz_quadrada(matriz[1])) or 1
     return numerador / denominador
 
