f923d1296a4dd0185bb58717f2f26fa39b7c38ba
Leonardo Alves <leunardosevla@gmail.com>
2017-11-10 01:51:57 -0200

    Adicionar documentacao e classe de amizade

--- Unico teste real da internet/server/integration/friendship-ml.js
+++ Unico teste real da internet/server/integration/friendship-ml.js
@@ -1,32 +1,51 @@
 const spawn = require('child_process').spawn;
 const path = '../../friendship-ml/proximidade.py';
-const userService = require('../service/userService');
-const tagService = require('../service/tagService');
-const quizService = require('../service/quizService');
-// const process = spawn('python3', [path]);
+const UserService = require('../service/userService');
+const DB = require('../factory/dbConnectionFactory')
+const TagService = require('../service/tagService');
+const QuizService = require('../service/quizService');
+const process = spawn('python3', [path]);
 
-let output = '';
+class Friendship {
 
-function montarDados (usuario, callback) {
-    let dado = [usuario];
-    userService.amigos(usuario, (amigos) => {
-        dado.push(amigos);
-        quizService.quizzesRespondidos(usuario, (quizzes) => {
-            dado.push(quizzes);
-            quizService.quizzesFeitos(usuario, (quizzes) => {
+    constructor () {
+        this._db = new DB();
+        this._tagService = new TagService(this._db);
+        this._userService = new UserService(this._db);
+        this._quizService = new QuizService(this._db);
+    }
+
+    /**
+     * Monta um array contendo os dados de um usuário.
+     * @param {*} usuario: Usuario aos quais os dados serão montados.
+     * @param {*} callback: Função executada após todas as informações serem pegas.
+     * @returns {Array} dados do usuario, no formato: [id, amigos, quizzes respondidos, quizzes feitos, tags]
+     */
+    montarDados(usuario, callback) {
+        let dado = [usuario];
+        this._userService.amigos(usuario, (amigos) => {
+            dado.push(amigos);
+            this._quizService.quizzesRespondidos(usuario, (quizzes) => {
                 dado.push(quizzes);
-                quizService.dispose();
-                tagService.buscarTagsUsuario(usuario, (tags) => {
-                    dado.push(tags);
-                    callback(dado);
-                    tagService.dispose();
+                this._quizService.quizzesFeitos(usuario, (quizzes) => {
+                    dado.push(quizzes);
+                    this._tagService.buscarTagsUsuario(usuario, (tags) => {
+                        dado.push(tags);
+                        callback(dado);
+                        this._db.dispose();
+                    })
+                    
                 })
             })
         })
-    })
-}
+    }
 
-montarDados(12, (dado) => console.log(dado));
+
+}
+let amizade = new Friendship();
+
+amizade.montarDados(12, (dados) => console.log(dados));
+// montarDados(12, (dado) => console.log(dado));
 // function buscarCombinacoes(idUsuario) {
 //     let possiveisAmigos = userService.sugestaoAmigos(idUsuario);
 //     for(usuario of possiveisAmigos) {
