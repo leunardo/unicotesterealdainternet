dd4a8b49deeca2331e083b73d5ae9f220b2aef29
Leonardo Alves <leunardosevla@gmail.com>
2017-11-10 00:59:01 -0200

    Adicionar classe factory de DB

--- Unico teste real da internet/server/factory/dbConnectionFactory.js
+++ Unico teste real da internet/server/factory/dbConnectionFactory.js
@@ -1,9 +1,26 @@
 const mysql = require('mysql');
-const connectionString = mysql.createConnection({
+const connectionString = {
     host     : 'localhost',
     user     : 'leo',
     password : '123',
     database : 'db_topquizzes'
-  });
+  };
 
-module.exports = mysql.createConnection(connectionString);
+class DB {
+  constructor () {
+    this._connection = mysql.createConnection(connectionString);
+  }
+
+  get connection () {
+    if (this._connection || this._connection.state === 'disconnected')
+      this._connection = mysql.createConnection(connectionString);
+    
+    return this._connection;
+  }
+
+  dispose () {
+    this._connection.end();
+  }
+}
+
+module.exports = DB;
