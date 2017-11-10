7f70b67b23e5c6bb62d330bd9c3861c2b62675ee
Leonardo Alves <leunardosevla@gmail.com>
2017-11-10 01:18:57 -0200

    Adicionar documentacao para classe query

--- Unico teste real da internet/server/query/query.js
+++ Unico teste real da internet/server/query/query.js
@@ -1,8 +1,20 @@
+const mysql = require('mysql');
+/**
+ * Classe base que faz chamadas ao banco de dados.
+ */
 class Query {
+
     constructor(connection) {
         this._connection = connection;
     }
 
+    /**
+     * Executa uma query e retorna o obtido por callback.
+     * 
+     * @param {Array} obj: parametros da query
+     * @param {*} callback: callback a ser executada caso não haja erro. 
+     * @param {*} query: query a ser executada. 
+     */
     executeQuery(obj, callback, query) {
         this._connection.query(query, obj, (err, result) => {
             if (err) throw err;
