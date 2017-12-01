b580732137bd7204b3f2d59f7717db4ca1d5bc81
guilhermeLS <guilhermejardimpereira@gmail.com>
2017-12-01 19:46:10 -0200

    Atualização do código responsável pelo display dos quizzes na home.
    AInda falta implementar as mesmas mudanças nas seguintes páginas: busca, perfis, menu de quizzes.

--- Unico teste real da internet/client/home/home.controller.js
+++ Unico teste real da internet/client/home/home.controller.js
@@ -1,6 +1,6 @@
 app.controller('homeController', homeController);
 
-function homeController($scope, quizService, authService) {  
+function homeController($scope, quizService, usuarioService, authService) {  
     $scope.logged = authService.isLogado();
     var quizzes = ["oi"];
     let i = 0;
@@ -17,25 +17,40 @@ function homeController($scope, quizService, authService) {
     function getRandomQuizzes(quizList){
         quizData = quizList.data;
         if(quizData.length>4){
-            for(var i=0; i<4; i++){
-                while(contem(id, idsUsados)||id==0){
-                    id = Math.floor(Math.random()*(quizData.length)+1);
-                }
-                idsUsados[i+1] = id;
-                quizService.getQuiz(id).then(mostrarQuiz)
-            }
+            quizSorting();
         }
         else{
             $scope.quizzes = quizData;
         }
     }
 
+    function quizSorting(){
+        while(contem(id, idsUsados)||id==0){
+            id = Math.floor(Math.random()*(quizData.length)+1);
+        }
+        idsUsados[i+1] = id;
+        quizService.getQuiz(id).then(mostrarQuiz).finally(getAutor);  
+    }
+
+    function getAutor(){
+        usuarioService.getUsuarioPorId(quizzes[i].id_usuario).then(assimilarAutor).finally(proximoQuiz);
+    }
+
     function mostrarQuiz(quiz){
         quizzes[i] = quiz.data[0];
+    }
+
+    function proximoQuiz(){
         i++;
         if(quizzes.length==4){
             $scope.quizzes = quizzes;
+        }else{
+            quizSorting();
         }
+    }
+
+    function assimilarAutor(user){
+        quizzes[i].autor = user.data[0];
     }
 
     function contem(id, idsUsados) {
